1cefeba3d81937f7f82704d9442230070c9c3c74
qwyuan <573011406@qq.com>
2022-11-01 21:19:52 +0800

    Window abstraction

---
+++ Razor/src/Razor/Window.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "rzpch.h"
+
+#include "Razor/Core.h"
+#include "Razor/Events/Event.h"
+namespace Razor
+{
+	struct WindowProps
+	{
+		std::string Title;
+		unsigned int Width;
+		unsigned int Height;
+
+		WindowProps(const std::string& title = "Razor Engine",
+			unsigned int width = 1280,
+			unsigned int height = 720)
+			:Title(title), Width(width), Height(height)
+		{
+		}
+	};
+
+	class RAZOR_API Window
+	{
+	public:
+		using EventCallbackFn = std::function<void(Event&)>;
+		virtual ~Window() {}
+
+		virtual void OnUpdate() = 0;
+		virtual unsigned int GetWidth() const = 0;
+		virtual unsigned int GetHeight() const = 0;
+
+		// Window attributes
+		virtual void SetEventCallback(const EventCallbackFn& callback) = 0;
+		virtual void SetVsync(bool enabled) = 0;
+		virtual void IsVsync() const = 0;
+
+		static Window* Create(const WindowProps& props = WindowProps());
+
+	};
+}
