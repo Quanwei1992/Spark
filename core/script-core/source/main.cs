1367a9393d86300bc4c0fa87451ae1525e5a8815
qwyuan <573011406@qq.com>
2023-02-07 18:55:41 +0800

    1. Move engine to core directory 2. Added script-core project

---
+++ core/script-core/source/main.cs
@@ -0,0 +1,27 @@
+
+
+using System;
+using System.Dynamic;
+namespace Spark
+{
+	public class Main
+	{
+		public float FloatVar { get; set; }
+
+		public Main()
+		{
+			Console.WriteLine("Main constructor");
+		}
+
+		public void PrintMessage()
+		{
+			Console.WriteLine("Hello World from C#");
+		}
+
+		public void PrintCustomMessage(string message)
+		{
+			Console.WriteLine($"C# says: {message}");
+		}
+	}
+
+}
